f731e5289ca4b8ce3185f50551ff238887e229e4
Ian McGillivary <wiian16@icloud.com>
2024-08-28 17:39:15 -0400

    Added blank lines to hello.c

---
+++ assignment1/hello.c
@@ -0,0 +1,18 @@
+/*
+ * Ian McGillivary
+ * hello.c
+ * This program will output three lines to the standard output:
+ * one with my full name,
+ * another with my favorite color,
+ * and another with my quest.
+ * 
+ * This program takes no inputs.
+ */
+
+#include <stdio.h>
+
+int main(void){
+    printf("NAME: Ian McGillivary\n\n");
+    printf("COLOR: Orange\n\n");
+    printf("QUEST: To learn the C language in the Unix environment\n\n")
+}
