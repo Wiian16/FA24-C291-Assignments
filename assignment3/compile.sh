27b5799e8ae6dd31003d54917e096a84585b31f8
Ian McGillivary <wiian16@icloud.com>
2024-09-25 17:32:56 -0400

    Added more tests

--- assignment3/compile.sh
+++ assignment3/compile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if gcc -g -rdynamic -o salary.out salary.c; then
+if gcc -g -fno-stack-protector -rdynamic -o salary.out salary.c; then
     echo "Compilation successful"
 else
     echo "Compilation failed"
