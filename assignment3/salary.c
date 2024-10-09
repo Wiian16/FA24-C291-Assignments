944bff9df4dc9a17b05b460677cc2b2afc67b806
Ian McGillivary <wiian16@icloud.com>
2024-10-08 21:35:02 -0400

    minor refactor

--- assignment3/salary.c
+++ assignment3/salary.c
@@ -1,19 +1,9 @@
-#include <regex.h>
 #include <stdbool.h>
-#include <string.h>
-#include <stdlib.h>
 #include <stdio.h>
 
 #define MONTHS_PER_YEAR 12
 #define WEEKS_PER_MONTH 4
 #define HOURS_PER_WEEK 40
-
-#define ADMIN 0
-#define STAFF 1 
-#define HOURLY 2
-#define ADJUNCT 3
-#define REGULAR 4
-#define TA 5
 
 const char usage[] = "Usage information: \n\
 Valid commands:\n\
@@ -34,168 +24,15 @@ Valid employee types:\n\
 \tT -- Teaching Assistant: Paid per course with regular and overtime hours\n\
 \t\tInput: Requires number of courses assisted and total hours worked in the month\n";
 
-/*
- * This function prints usage instructions to stdout
- */
+
 void printUsage(void){
     printf("%s", usage);
 }
 
-/*
- * This method takes in the user input after validation, and returns the type of employee based on the
- * character provided. If the character is invalid, -1 is returned. 
- */
-int getType(char typeChar){
-    if (typeChar == 'A'){
-        return ADMIN;
-    }
-    else if (typeChar == 'S'){
-        return STAFF;
-    }
-    else if (typeChar == 'J'){
-        return ADJUNCT;
-    }
-    else if (typeChar == 'R'){
-        return REGULAR;
-    }
-    else if (typeChar == 'T'){
-        return TA;
-    }
-    else if (typeChar == 'H'){
-        return HOURLY;
-    }
-    else{
-        return -1;
-    }
-}
 
 /*
- * This method takes in two strings (char arrays), appends them together, and returns the new array
- */ 
-char * append(char * a, char * b){
-    int len = strlen(a) + strlen(b) + 1;
-
-    char * str = (char *) malloc(len);
-
-    strcpy(str, a);
-    strcat(str, b);
-
-    return str;
-}
-
-/*
- * This function outputs a formatted string of the admin average salary when given the yearly salary
- */
-char * calculateAdmin(float salary){ 
-    char * str = (char *) malloc(60);
-    sprintf(str, "%-18s%-17.2f%-.2f\n", "Administrator", salary / MONTHS_PER_YEAR, salary / MONTHS_PER_YEAR);
-    return str;
-}
-
-/*
- * This function outputs a formatted string of the staff average salary and pay when given yearly salary and
- * overtime hours
- */
-char * calculateStaff(float salary, float overtimeHours){
-    char * str = (char *) malloc(60);
-
-    float avgSalary = salary / MONTHS_PER_YEAR;
-    float hourly = avgSalary / WEEKS_PER_MONTH / HOURS_PER_WEEK;
-    float overtimeHourly = hourly * 1.5;
-
-    sprintf(str, "%-18s%-17.2f%-.2f\n", "Staff", avgSalary, avgSalary + overtimeHourly * overtimeHours);
-    return str;
-}
-
-/*
- * This function outputs a formatted string of the hourly average salary and pay when given an hourly pay rate
- * and hours worked. Hourly workers are paid at their base rate for the first 20 hours, then 1.25 times their 
- * rate for the next 20.
- */
-char * calculateHourly(float payRate, float hoursWorked){
-    const int HOURS_PER_MONTH = HOURS_PER_WEEK / 2 * WEEKS_PER_MONTH; //20 hours per week for one month
-    const float OVERTIME_RATE = 1.25;
-
-    float regularHours, overtimeHours;
-
-    if(hoursWorked > HOURS_PER_MONTH){
-        regularHours = HOURS_PER_MONTH;
-        hoursWorked -= HOURS_PER_MONTH;
-    }
-    else{
-        regularHours = hoursWorked;
-        hoursWorked = 0;
-    }
-
-    if(hoursWorked > HOURS_PER_MONTH){
-        overtimeHours = HOURS_PER_MONTH;
-    }
-    else{
-        overtimeHours = hoursWorked;
-    }
-
-    char * str = (char *) malloc(60);
-    
-    float avgSalary = payRate * HOURS_PER_MONTH; 
-    sprintf(str, "%-18s%-17.2f%-.2f\n", "Hourly", avgSalary, avgSalary + payRate * OVERTIME_RATE * overtimeHours);
-    return str;
-}
-
-/*
- * This function outputs a formatted string of the adjunct average salary and pay when given their credit houts taught.
- */
-char * calculateAdjunct(float creditHours){
-    const float ADJUNCT_RATE = 8500.0 / 3;
-    const int ADJUNCT_MONTHS = 5;
-
-    int hours = (int) creditHours;
-
-    float avg_salary = hours * ADJUNCT_RATE / ADJUNCT_MONTHS;
-
-    char * str = (char *) malloc(60);
-    sprintf(str, "%-18s%-17.2f%-.2f\n", "Adjunct", (float) avg_salary, avg_salary);
-    return str;
-}
-
-/*
- * This function outputs a formatted string of regular faculty average salary and pay when given their annual salary
- * and credit hour overload
- */
-char * calculateRegular(float annualSalary, float creditOverload){
-    const float OVERLOAD_RATE = 8500.0 / 3;
-    const int OVERLOAD_MONTHS = 5;
-    const int SALARY_MONTHS = 10;
-
-    float avgSalary = annualSalary / SALARY_MONTHS;
-    float avgOverloadPay = creditOverload * OVERLOAD_RATE / OVERLOAD_MONTHS;
-
-    char * str = (char *) malloc(60);
-    sprintf(str, "%-18s%-17.2f%-.2f\n", "Faculty", avgSalary, avgSalary + avgOverloadPay);
-    return str;
-}
-
-
-char * calculateTA(float courses, float hours){ //todo
-    const int REGULAR_HOURS = 10;
-    const int OVERTIME_HOURS = 2;
-    const int MONTHS = 5;
-    const float MONTHLY_RATE = 2500.0 / MONTHS;
-    const float HOURLY_RATE = MONTHLY_RATE / WEEKS_PER_MONTH / 10;
-    
-    int maxHours = REGULAR_HOURS * WEEKS_PER_MONTH * courses + OVERTIME_HOURS * WEEKS_PER_MONTH * courses;
-    
-    if(hours > maxHours){
-        hours = maxHours;
-    }
-
-    float pay = hours * HOURLY_RATE;
-
-    char * str = (char *) malloc(60);
-    sprintf(str, "%-18s%-17.2f%-.2f\n", "TA", pay, pay);
-    return str;
-}
-
-/*
+ * Author -- Ian McGillivary
+ * Program -- University Payroll Calculator
  * This function runs on program start, it first initializes a regular expression for validation of user input.
  * Second, the program will enter a loop until the user quits the program. During this loop, it will wait for user
  * input, if this input is 'q', the loop will exit, if the input is '?', it will print instructions, and otherwise,
@@ -206,124 +43,205 @@ char * calculateTA(float courses, float hours){ //todo
  * data.
  */
 int main(void){
-    char * output = "EMPLOYEE_TYPE     AVG_SALARY       AVG_PAY\n";
+    //admin values
+    double admin_total_salary = 0, admin_total_pay = 0, admin_count = 0;
+    //staff values
+    double staff_total_salary = 0, staff_total_pay = 0, staff_count = 0;
+    //adjunct values
+    double adjunct_total_salary = 0, adjunct_total_pay = 0, adjunct_count = 0;
+    //regular values
+    double regular_total_salary = 0, regular_total_pay = 0, regular_count = 0;
+    //ta values
+    double ta_total_salary = 0, ta_total_pay = 0, ta_count = 0;
+    //hourly values
+    double hourly_total_salary = 0, hourly_total_pay = 0, hourly_count = 0;
 
     while(true){
-        char type;
+        char type = 0;
 
-        printf("Enter employee salary:\n");
-        scanf(" %c ", &type);
-        printf("type=%c\n", type);
-        //break loop and print output
+        printf("Enter employee salary: ");
+        scanf(" %c", &type);
+        
         if(type == 'q' || type == 'Q'){
             break;
         }
-        //print instructions and continue
+
         if(type == '?'){
             printUsage();
             continue;
         }
 
-        int typeCode = getType(type);
+        double param1 = 0, param2 = 0;
 
-        double param1 = 0;
-        double param2 = 0;
-        int params = 0;
+        switch(type){
+            case 'A': { //Administrator
+                          scanf("%lf", &param1);
 
-        switch(typeCode){
-            case ADMIN: 
-                params = scanf(" %lf ", &param1);
-                printf("param1=%f\n", param1);
-    
-                if(params != 1){
-                    printf("Invalid input, enter '?' to see usage\n");
-                    continue;
-                }
+                          double salary = param1 / MONTHS_PER_YEAR;
 
-                output = append(output, calculateAdmin(param1));
+                          admin_total_salary += salary;
+                          admin_total_pay += salary;
+                          admin_count++;
 
-                break;
-            case STAFF:
-                params = scanf(" %lf %lf ", &param1, &param2);
+                          break;
+                      }
+            case 'S': { //Staff
+                          scanf("%lf %lf", &param1, &param2);
 
-                if(params != 2){
-                    printf("Invalid input, enter '?' to see usage\n");
-                    continue;
-                }
+                          if(param2 > 10){
+                              printf("Warning: Staff member over allowed overtime hours, extra time discarded\n");
+                              param2 = 10;
+                          }
 
-                if(param2 > 10){
-                    printf("Warning: staff member above allowed overtime hours, data discarded\n");
-                    continue;
-                }
+                          const float OVERTIME_RATE = 1.5;
 
-                output = append(output, calculateStaff(param1, param2));
+                          double salary = param1 / MONTHS_PER_YEAR;
+                          double hourlyRate = salary / WEEKS_PER_MONTH / HOURS_PER_WEEK;
+                          double overtimeHourly = hourlyRate * OVERTIME_RATE;
 
-                break;
-            case HOURLY:
-                params = scanf(" %lf %lf", &param1, &param2);
+                          double pay = salary;
+                          double overtimePay = overtimeHourly * param2;
 
-                if(params != 2){
-                    printf("Invalid input, enter '?' to see usage\n");
-                    continue;
-                }
+                          pay += overtimePay;
 
-                if(param2 > 20){
-                    printf("Warning: hourly employee over allowed time, data discarded\n");
-                    continue;
-                }
+                          staff_total_salary += salary;
+                          staff_total_pay += pay;
+                          staff_count++;
 
-                output = append(output, calculateHourly(param1, param2));
+                          break;
+                      }
+            case 'H': { //Hourly
+                          scanf("%lf %lf", &param1, &param2);
 
-                break;
-            case ADJUNCT:
-                params = scanf(" %lf ", &param1);
+                          if(param2 > 20 * WEEKS_PER_MONTH + 20){
+                              printf("Warning: Hourly employee over allowed overtime hours, extra time discarded\n");
+                              param2 = 20 * WEEKS_PER_MONTH + 20;
+                          }
 
-                if(params != 1){
-                    printf("Invalid input, enter '?' to see usage\n");
-                    continue;
-                }
+                          const int HOURS_PER_MONTH = 80;
+                          const float OVERTIME_RATE = 1.25;
 
-                if(param1 > 9){
-                    printf("Warning: adjuncty faculty member over allowed credit hours, data discarded\n");
-                    continue;
-                }
+                          int hours = 0, overtimeHours = 0;
 
-                output = append(output, calculateAdjunct(param1));
+                          if(param2 > HOURS_PER_MONTH){
+                              hours = HOURS_PER_MONTH;
+                              overtimeHours = param2 - hours;
+                          }
+                          else{
+                              hours = param2;
+                          }
 
-                break;
-            case REGULAR:
-                params = scanf(" %lf %lf ", &param1, &param2);
+                          double salary = hours * param1;
+                          double overtimePay = overtimeHours * param1 * OVERTIME_RATE;
+                          double pay = salary + overtimePay;
 
-                if(params != 2){
-                    printf("Invalid input, enter '?' to see usage\n");
-                    continue;
-                }
+                          hourly_total_salary += salary;
+                          hourly_total_pay += pay;
+                          hourly_count++;
 
-                output = append(output, calculateRegular(param1, param2));                
+                          break;
+                      }
+            case 'J': { //Adjunct
+                          scanf("%lf", &param1);
 
-                break;
-            case TA:
-                params = scanf(" %lf %lf ", &param1, &param2);
+                          if(param1 > 9){
+                              printf("Warning: Adjunct instructor over allowed credit hours, extra credits discarded\n");
+                              param1 = 9;
+                          }
 
-                if(params != 2){
-                    printf("Invalid input, enter '?' to see usage\n");
-                    continue;
-                }
+                          const float ADJUNCT_RATE = 8500.0 / 3;
+                          const int MONTHS = 5;
 
-                int maxHours = param1 * 12 * WEEKS_PER_MONTH * 5;
+                          int hours = (int) param1;
 
-                if(param2 > maxHours){
-                    printf("Warning: teaching assistant over allowed hours, data discarded\n");
-                    continue;
-                }
+                          float salary = hours * ADJUNCT_RATE / MONTHS;
 
-                output = append(output, calculateTA(param1, param2));
+                          adjunct_total_salary += salary;
+                          adjunct_total_pay += salary;
+                          adjunct_count++;
 
-                break;
+                          break;
+                      }
+            case 'R': { //Regular Faculty
+                          scanf(" %lf %lf", &param1, &param2);
+                          const int MONTHS = 10;
+                          const int OVERLOAD_MONTHS = 5;
+                          const float OVERLOAD_RATE = 8500.0 / 3;
+
+                          float salary = param1 / MONTHS;
+                          float overloadPay = param2 * OVERLOAD_RATE / OVERLOAD_MONTHS;
+                          float pay = salary + overloadPay;
+                          
+                          regular_total_salary += salary;
+                          regular_total_pay += pay;
+                          regular_count++;
+
+                          break;
+                      }
+            case 'T': { //TA
+                          scanf("%lf %lf", &param1, &param2);
+                          
+                          const float COURSE_RATE = 2500.0;
+                          const int TA_HOURS_PER_WEEK = 10;
+                          const int EXPECTED_HOURS = WEEKS_PER_MONTH * TA_HOURS_PER_WEEK * param1;
+                          const int MAX_HOURS = EXPECTED_HOURS + 2 * WEEKS_PER_MONTH;
+                          const float HOURLY_RATE = COURSE_RATE / 5 / EXPECTED_HOURS * param1;
+
+                          if(param2 > MAX_HOURS){
+                              printf("Warning: TA over maximum overtime hours, extra time discarded.\n");
+                              param2 = MAX_HOURS;
+                          }
+
+                          float hours = 0, overtimeHours = 0;
+
+                          if(param2 > EXPECTED_HOURS){
+                              hours = EXPECTED_HOURS;
+                              overtimeHours = param2 - EXPECTED_HOURS;
+                          }
+                          else{
+                              hours = param2;
+                          }
+                          
+                          float salary = HOURLY_RATE * hours;
+                          float overtimePay = overtimeHours * HOURLY_RATE;
+                          float pay = salary + overtimePay;
+
+                          ta_total_salary += salary;
+                          ta_total_pay += pay;
+                          ta_count++;
+
+                          break;
+                      }
             default:
                 printf("Invalid input, enter '?' to see usage\n");
+                continue;
         }
     }
 
-    printf("%s\n", output);
+    //Admin
+    double admin_avg_salary = (admin_count == 0) ? 0 : admin_total_salary / admin_count;
+    double admin_avg_pay = (admin_count == 0) ? 0 : admin_total_pay / admin_count;
+    //Staff
+    double staff_avg_salary = (staff_count == 0) ? 0 : staff_total_salary / staff_count;
+    double staff_avg_pay = (staff_count == 0) ? 0 : staff_total_pay / staff_count;
+    //Adjunct
+    double adjunct_avg_salary = (adjunct_count == 0) ? 0 : adjunct_total_salary / adjunct_count;
+    double adjunct_avg_pay = (adjunct_count == 0) ? 0 : adjunct_total_pay / adjunct_count;
+    //Regular
+    double regular_avg_salary = (regular_count == 0) ? 0 : regular_total_salary / regular_count;
+    double regular_avg_pay = (regular_count == 0) ? 0 : regular_total_pay / regular_count;
+    //Hourly
+    double hourly_avg_salary = (hourly_count == 0) ? 0 : hourly_total_salary / hourly_count;
+    double hourly_avg_pay = (hourly_count == 0) ? 0 : hourly_total_pay / hourly_count;
+    //TA
+    double ta_avg_salary = (ta_count == 0) ? 0 : ta_total_salary / ta_count;
+    double ta_avg_pay = (ta_count == 0) ? 0 : ta_total_pay / ta_count;
+
+    printf("EMPLOYEE_TYPE     AVG_SALARY       AVG_PAY\n");
+    printf("%-18s%-17.2f%-.2f\n", "Administrator", admin_avg_salary, admin_avg_pay);
+    printf("%-18s%-17.2f%-.2f\n", "Staff", staff_avg_salary, staff_avg_pay);
+    printf("%-18s%-17.2f%-.2f\n", "Faculty", regular_avg_salary, regular_avg_pay);
+    printf("%-18s%-17.2f%-.2f\n", "Adjunct", adjunct_avg_salary, adjunct_avg_pay);
+    printf("%-18s%-17.2f%-.2f\n", "TA", ta_avg_salary, ta_avg_pay);
+    printf("%-18s%-17.2f%-.2f\n", "Hourly", hourly_avg_salary, hourly_avg_pay);
 }
