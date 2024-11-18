2e515595aec784c5050f39bf190f9f96fb5a901f
Ian McGillivary <wiian16@icloud.com>
2024-11-18 17:12:03 -0500

    Adding extra files from assignment 5

---
+++ assignment5/scratch.py
@@ -0,0 +1,1 @@
+print("This is a test for handling NULL \0values correctly. These values may be unexpected")
---
+++ assignment5/test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+./run.sh << EOF
+All the men and all the women in all the world.
+EOF
