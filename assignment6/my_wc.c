62314520bd7c2265e462fcaafadce9f377d66a3b
Ian McGillivary <wiian16@icloud.com>
2024-11-18 23:39:21 -0500

    Finished line and word count

--- assignment6/my_wc.c
+++ assignment6/my_wc.c
@@ -14,15 +14,35 @@
 #define AVERAGE_CHARACTER_COUNT 'a'
 #define EXCLUDE_PUNCTUATION 'p'
 
+#define DEFAULT_BUFFER_SIZE 40
+#define DEFAULT_WORD_LIST_SIZE 5
+
 
 void parseArgs(int, char **);
 void printHelp();
 void debugLog(char *);
+struct inputBuffer fillBuffer();
+struct inputBuffer resize(struct inputBuffer);
+void report(struct inputBuffer);
+struct wordList fillWordList(struct inputBuffer);
+int indexOf(struct wordList, char *);
 
 
 struct inputBuffer {
     size_t size;
+    size_t maxSize;
     char * buffer;
+};
+
+struct word {
+    size_t length;
+    size_t count;
+    char * word;
+};
+
+struct wordList {
+    size_t length;
+    struct word * list;
 };
 
 
@@ -34,21 +54,17 @@ bool debug = false;
 bool averageCharCount = false;
 bool excludePunct = false;
 
+char * delim = " \t\n\r";
+
 
 int main(int argc, char ** argv){
     parseArgs(argc, argv);
 
-    printf("lineCount: %d\n", lineCount);
-    printf("characterCount: %d\n", characterCount);
-    printf("wordCount: %d\n", wordCount);
-    printf("frequency: %d\n", frequency);
-    printf("debug: %d\n", debug);
-    printf("averageCharCount: %d\n", averageCharCount);
-    printf("excludePunct: %d\n", excludePunct);
+    struct inputBuffer buf = fillBuffer();
 
-    debugLog("Debug log test");
+    printf("buf: %s\n", buf.buffer);
 
-    
+    report(buf);
 }
 
 
@@ -57,7 +73,6 @@ void parseArgs(int argc, char ** argv){
         lineCount = true;
         wordCount = true;
         characterCount = true;
-        return;
     }
 
     for(int i = 1; i < argc; i++){
@@ -112,6 +127,14 @@ void parseArgs(int argc, char ** argv){
             }
         }
     }
+
+    if(lineCount) debugLog("Flag set: Line count");
+    if (wordCount) debugLog("Flag set: Word count");
+    if (characterCount) debugLog("Flag set: Character count");
+    if (frequency) debugLog("Flag set: Word frequency");
+    if (debug) debugLog("Flag set: Debug");
+    if (averageCharCount) debugLog("Flag set: Average character count");
+    if (excludePunct) debugLog("Flag set: Exclude punctuation");
 }
 
 
@@ -144,4 +167,115 @@ void debugLog(char * msg){
     if(debug){
         fprintf(stderr, "DEBUG    %s\n", msg);
     }
+}
+
+
+struct inputBuffer fillBuffer(){
+    struct inputBuffer buf;
+    buf.buffer = (char *) calloc(DEFAULT_BUFFER_SIZE, sizeof(char));
+    buf.size = 0;
+    buf.maxSize = DEFAULT_BUFFER_SIZE;
+
+    while(!feof(stdin)){
+        if(!fread(buf.buffer + buf.size, 1, sizeof(char), stdin)){
+            break;
+        }
+
+        buf.size++;
+
+        if(buf.size == buf.maxSize){
+            buf = resize(buf);
+        }
+    }
+
+    return buf;
+}
+
+
+struct inputBuffer resize(struct inputBuffer buf){
+    char * newBuffer = realloc(buf.buffer, buf.maxSize + DEFAULT_BUFFER_SIZE);
+
+    for(int i = buf.size; i < buf.maxSize + DEFAULT_BUFFER_SIZE; i++){
+        newBuffer[i] = 0;
+    }
+
+    buf.buffer = newBuffer;
+    buf.maxSize += DEFAULT_BUFFER_SIZE;
+
+    return buf;
+}
+
+
+void report(struct inputBuffer buf) {
+    if(lineCount){
+        int lines = 0;
+
+        for(int i = 0; i < buf.size; i++){
+            if(buf.buffer[i] == '\n'){
+                lines++;
+            }
+        }
+
+        printf("line count : %d\n", lines);
+    } 
+
+    struct wordList list = fillWordList(buf);
+
+    if(wordCount){
+        int words = 0;
+
+        for(int i = 0; i < list.length; i++){
+            words += list.list[i].count;
+        } 
+
+        printf("word count : %d\n", words);
+    }
+}
+
+
+struct wordList fillWordList(struct inputBuffer buf){
+    debugLog("Starting word list fill");
+
+    struct wordList list;
+    list.length = 0;
+    list.list = (struct word *) calloc(DEFAULT_WORD_LIST_SIZE, sizeof(struct word));
+
+    char * token = strtok(buf.buffer, delim);
+
+    while(token != NULL) {
+        int index = indexOf(list, token);
+
+        if(index != -1){
+            list.list[index].count++;
+        }
+        else {
+            struct word newWord;
+            newWord.count = 1;
+            newWord.length = strlen(token);
+            newWord.word = token;
+
+            list.list[list.length] = newWord;
+            list.length++;
+        }
+
+        token = strtok(NULL, delim);
+    }
+
+    debugLog("Done filling word list");
+
+    return list;
+} 
+
+
+int indexOf(struct wordList list, char * str){
+    int index = -1;
+
+    for(int i = 0; i < list.length; i++){
+        if(strcmp(list.list[i].word, str) == 0){
+            index = i;
+            break;
+        }
+    }
+
+    return index;
 }
